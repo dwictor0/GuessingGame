21786ca25b90ac78d5041da961599c2a4026adb3
dwictor0 <daniel.wdev0@gmail.com>
2026-04-03 00:14:02 -0300

    FEAT: definindo variavel numero aleatorio

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,6 +3,7 @@ use rand::Rng;
 
 fn main() {
     println!("Guessing Game");
+    let secret_number = rand::rng().random_range(1..=100);
     println!("Digite um numero: ");
 
     let mut guess = String::new();
