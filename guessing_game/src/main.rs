429465a0b17de8f7fe9b7b70193f1aaee717aab0
dwictor0 <daniel.wdev0@gmail.com>
2026-04-03 00:26:21 -0300

    FIX: padronizando mensagens de feedback em portugues

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -12,9 +12,9 @@ fn main() {
     let guess: u32 =  guess.trim().parse().expect("Digite um numero valido!");
     println!("Numero digitado: {guess}");
      match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win"),
+        Ordering::Less => println!("O numero secreto e menor."),
+        Ordering::Greater => println!("O numero secreto e maior."),
+        Ordering::Equal => println!("Acertou!"),
     }
    
 }
