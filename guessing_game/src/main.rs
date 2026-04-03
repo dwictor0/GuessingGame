0d83e6f486d77aa562582ff8e9a5a65b6de49463
dwictor0 <daniel.wdev0@gmail.com>
2026-04-03 00:32:50 -0300

    FEAT: adicionando loop para tentativas

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,16 +5,17 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guessing Game");
     let secret_number = rand::rng().random_range(1..=100);
-    println!("Digite um numero: ");
-
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Erro ao ler a linha informada.");
-    let guess: u32 =  guess.trim().parse().expect("Digite um numero valido!");
-    println!("Numero digitado: {guess}");
-     match guess.cmp(&secret_number) {
-        Ordering::Less => println!("O numero secreto e maior."),
-        Ordering::Greater => println!("O numero secreto e menor."),
-        Ordering::Equal => println!("Acertou!"),
+    loop {
+        println!("Digite um numero: ");
+        
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Erro ao ler a linha informada.");
+        let guess: u32 =  guess.trim().parse().expect("Digite um numero valido!");
+        println!("Numero digitado: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("O numero secreto e maior."),
+            Ordering::Greater => println!("O numero secreto e menor."),
+            Ordering::Equal => println!("Acertou!"),
+        }
     }
-   
 }
