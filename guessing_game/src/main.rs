2539bc766f5d65bbd81fc176f6aa9b13e1122415
dwictor0 <daniel.wdev0@gmail.com>
2026-04-02 23:57:54 -0300

    FEAT: atualizando funcao main guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,11 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing Game");
+    println!("Digite um numero: ");
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Erro ao ler a linha informada.");
+
+    println!("Numero digitado: {guess}")
 }
